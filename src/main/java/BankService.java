f11ae325d64f7c8563d2a29a408f6e98090cdfee
Khaly_Ruot <khalyruot12345@gmail.com>
2023-02-21 17:38:00 +0000

    Done

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        balance += amount;
     }
 
     /**
@@ -32,7 +32,14 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if(balance >= amount){
+            balance -= amount;
+        }
+        else {
+            amount = 0;
+            balance -=amount; 
+    
+        }
     }
 
     /**
@@ -40,6 +47,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
